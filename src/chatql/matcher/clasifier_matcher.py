db750178488f9d0f5add6c497a13487132e8fe38
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-01-19 23:53:22 +0900

    Remove Flags from classifier_matcher

--- src/chatql/matcher/clasifier_matcher.py
+++ src/chatql/matcher/clasifier_matcher.py
@@ -20,97 +20,32 @@ import os
 from chatql.matcher.bert import modeling, optimization, tokenization
 import tensorflow as tf
 
-flags = tf.flags
-
-FLAGS = flags.FLAGS
 
 # Required parameters
-flags.DEFINE_string(
-        "data_dir", None,
-        "The input data dir. Should contain the .tsv files (or other data files) "
-        "for the task.")
-
-flags.DEFINE_string(
-        "bert_config_file", None,
-        "The config json file corresponding to the pre-trained BERT model. "
-        "This specifies the model architecture.")
-
-flags.DEFINE_string("task_name", None, "The name of the task to train.")
-
-flags.DEFINE_string("vocab_file", None,
-                    "The vocabulary file that the BERT model was trained on.")
-
-flags.DEFINE_string(
-        "output_dir", None,
-        "The output directory where the model checkpoints will be written.")
+data_dir = None
+bert_config_file = None
+vocab_file = None
+output_dir = None
+init_checkpoint = None
 
 # Other parameters
+max_seq_length = 128
+train_batch_size = 32
+eval_batch_size = 32
+predict_batch_size = 8
+learning_rate = 5e-5
+num_train_epochs = 3.0
+warmup_proportion = 0.1
+save_checkpoints_steps = 1000
+iterations_per_loop = 1000
 
-flags.DEFINE_string(
-        "init_checkpoint", None,
-        "Initial checkpoint (usually from a pre-trained BERT model).")
-
-flags.DEFINE_integer(
-        "max_seq_length", 128,
-        "The maximum total input sequence length after WordPiece tokenization. "
-        "Sequences longer than this will be truncated, and sequences shorter "
-        "than this will be padded.")
-
-flags.DEFINE_bool("do_train", False, "Whether to run training.")
-
-flags.DEFINE_bool("do_eval", False, "Whether to run eval on the dev set.")
-
-flags.DEFINE_bool(
-        "do_predict", False,
-        "Whether to run the model in inference mode on the test set.")
-
-flags.DEFINE_integer("train_batch_size", 32, "Total batch size for training.")
-
-flags.DEFINE_integer("eval_batch_size", 8, "Total batch size for eval.")
-
-flags.DEFINE_integer("predict_batch_size", 8, "Total batch size for predict.")
-
-flags.DEFINE_float("learning_rate", 5e-5, "The initial learning rate for Adam.")
-
-flags.DEFINE_float("num_train_epochs", 3.0,
-                   "Total number of training epochs to perform.")
-
-flags.DEFINE_float(
-        "warmup_proportion", 0.1,
-        "Proportion of training to perform linear learning rate warmup for. "
-        "E.g., 0.1 = 10% of training.")
-
-flags.DEFINE_integer("save_checkpoints_steps", 1000,
-                     "How often to save the model checkpoint.")
-
-flags.DEFINE_integer("iterations_per_loop", 1000,
-                     "How many steps to make in each estimator call.")
-
-flags.DEFINE_bool("use_tpu", False, "Whether to use TPU or GPU/CPU.")
-
-tf.flags.DEFINE_string(
-        "tpu_name", None,
-        "The Cloud TPU to use for training. This should be either the name "
-        "used when creating the Cloud TPU, or a grpc://ip.address.of.tpu:8470 "
-        "url.")
-
-tf.flags.DEFINE_string(
-        "tpu_zone", None,
-        "[Optional] GCE zone where the Cloud TPU is located in. If not "
-        "specified, we will attempt to automatically detect the GCE project from "
-        "metadata.")
-
-tf.flags.DEFINE_string(
-        "gcp_project", None,
-        "[Optional] Project name for the Cloud TPU-enabled project. If not "
-        "specified, we will attempt to automatically detect the GCE project from "
-        "metadata.")
-
-tf.flags.DEFINE_string("master", None, "[Optional] TensorFlow master URL.")
-
-flags.DEFINE_integer(
-        "num_tpu_cores", 8,
-        "Only used if `use_tpu` is True. Total number of TPU cores to use.")
+# TPU COnfiguration
+use_tpu = False
+tpu_name = None
+tpu_zone = None
+gcp_project = None
+master = None
+num_tpu_cores = 8
 
 
 class InputExample(object):
@@ -757,7 +692,7 @@ def convert_examples_to_features(examples, label_list, max_seq_length, tokenizer
     return features
 
 
-def main(_):
+def train(_):
     tf.logging.set_verbosity(tf.logging.INFO)
 
     processors = {
@@ -767,90 +702,80 @@ def main(_):
             "xnli": XnliProcessor,
     }
 
-    tokenization.validate_case_matches_checkpoint(True, FLAGS.init_checkpoint)
+    tokenization.validate_case_matches_checkpoint(True, init_checkpoint)
 
-    if not FLAGS.do_train and not FLAGS.do_eval and not FLAGS.do_predict:
-        raise ValueError(
-                "At least one of `do_train`, `do_eval` or `do_predict' must be True.")
+    bert_config = modeling.BertConfig.from_json_file(bert_config_file)
 
-    bert_config = modeling.BertConfig.from_json_file(FLAGS.bert_config_file)
-
-    if FLAGS.max_seq_length > bert_config.max_position_embeddings:
+    if max_seq_length > bert_config.max_position_embeddings:
         raise ValueError(
                 "Cannot use sequence length %d because the BERT model "
                 "was only trained up to sequence length %d" %
-                (FLAGS.max_seq_length, bert_config.max_position_embeddings))
+                (max_seq_length, bert_config.max_position_embeddings))
 
-    tf.gfile.MakeDirs(FLAGS.output_dir)
+    tf.gfile.MakeDirs(output_dir)
 
-    task_name = FLAGS.task_name.lower()
-
-    if task_name not in processors:
-        raise ValueError("Task not found: %s" % (task_name))
-
-    processor = processors[task_name]()
+    processor = None  # TODO: ちゃんとしたprocessorを指定する
 
     label_list = processor.get_labels()
 
     tokenizer = tokenization.FullTokenizer(
-            vocab_file=FLAGS.vocab_file, do_lower_case=True)
+            vocab_file=vocab_file, do_lower_case=True)
 
     tpu_cluster_resolver = None
-    if FLAGS.use_tpu and FLAGS.tpu_name:
+    if use_tpu and tpu_name:
         tpu_cluster_resolver = tf.contrib.cluster_resolver.TPUClusterResolver(
-                FLAGS.tpu_name, zone=FLAGS.tpu_zone, project=FLAGS.gcp_project)
+                tpu_name, zone=tpu_zone, project=gcp_project)
 
     is_per_host = tf.contrib.tpu.InputPipelineConfig.PER_HOST_V2
     run_config = tf.contrib.tpu.RunConfig(
             cluster=tpu_cluster_resolver,
-            master=FLAGS.master,
-            model_dir=FLAGS.output_dir,
-            save_checkpoints_steps=FLAGS.save_checkpoints_steps,
+            master=master,
+            model_dir=output_dir,
+            save_checkpoints_steps=save_checkpoints_steps,
             tpu_config=tf.contrib.tpu.TPUConfig(
-                    iterations_per_loop=FLAGS.iterations_per_loop,
-                    num_shards=FLAGS.num_tpu_cores,
+                    iterations_per_loop=iterations_per_loop,
+                    num_shards=num_tpu_cores,
                     per_host_input_for_training=is_per_host))
 
     train_examples = None
     num_train_steps = None
     num_warmup_steps = None
-    if FLAGS.do_train:
-        train_examples = processor.get_train_examples(FLAGS.data_dir)
-        num_train_steps = int(
-                len(train_examples) / FLAGS.train_batch_size * FLAGS.num_train_epochs)
-        num_warmup_steps = int(num_train_steps * FLAGS.warmup_proportion)
+
+    train_examples = processor.get_train_examples(data_dir)
+    num_train_steps = int(
+            len(train_examples) / train_batch_size * num_train_epochs)
+    num_warmup_steps = int(num_train_steps * warmup_proportion)
 
     model_fn = model_fn_builder(
             bert_config=bert_config,
             num_labels=len(label_list),
-            init_checkpoint=FLAGS.init_checkpoint,
-            learning_rate=FLAGS.learning_rate,
+            init_checkpoint=init_checkpoint,
+            learning_rate=learning_rate,
             num_train_steps=num_train_steps,
             num_warmup_steps=num_warmup_steps,
-            use_tpu=FLAGS.use_tpu,
-            use_one_hot_embeddings=FLAGS.use_tpu)
+            use_tpu=use_tpu,
+            use_one_hot_embeddings=use_tpu)
 
     # If TPU is not available, this will fall back to normal Estimator on CPU
     # or GPU.
     estimator = tf.contrib.tpu.TPUEstimator(
-            use_tpu=FLAGS.use_tpu,
+            use_tpu=use_tpu,
             model_fn=model_fn,
             config=run_config,
-            train_batch_size=FLAGS.train_batch_size,
-            eval_batch_size=FLAGS.eval_batch_size,
-            predict_batch_size=FLAGS.predict_batch_size)
+            train_batch_size=train_batch_size,
+            eval_batch_size=eval_batch_size,
+            predict_batch_size=predict_batch_size)
 
-    if FLAGS.do_train:
-        train_file = os.path.join(FLAGS.output_dir, "train.tf_record")
-        file_based_convert_examples_to_features(
-                train_examples, label_list, FLAGS.max_seq_length, tokenizer, train_file)
-        tf.logging.info("***** Running training *****")
-        tf.logging.info("  Num examples = %d", len(train_examples))
-        tf.logging.info("  Batch size = %d", FLAGS.train_batch_size)
-        tf.logging.info("  Num steps = %d", num_train_steps)
-        train_input_fn = file_based_input_fn_builder(
-                input_file=train_file,
-                seq_length=FLAGS.max_seq_length,
-                is_training=True,
-                drop_remainder=True)
-        estimator.train(input_fn=train_input_fn, max_steps=num_train_steps)
+    train_file = os.path.join(output_dir, "train.tf_record")
+    file_based_convert_examples_to_features(
+            train_examples, label_list, max_seq_length, tokenizer, train_file)
+    tf.logging.info("***** Running training *****")
+    tf.logging.info("  Num examples = %d", len(train_examples))
+    tf.logging.info("  Batch size = %d", train_batch_size)
+    tf.logging.info("  Num steps = %d", num_train_steps)
+    train_input_fn = file_based_input_fn_builder(
+            input_file=train_file,
+            seq_length=max_seq_length,
+            is_training=True,
+            drop_remainder=True)
+    estimator.train(input_fn=train_input_fn, max_steps=num_train_steps)
